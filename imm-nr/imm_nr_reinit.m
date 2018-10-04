5d775374ce96e62b8ed11ed7d4cee568905068a4
Yang Wang <wang701@purdue.edu>
2018-10-04 17:21:17 -0400

    Remove: simulation files

--- imm-nr/imm_nr_reinit.m
+++ imm-nr/imm_nr_reinit.m
@@ -1,9 +1,10 @@
 function [x_0j, P_0j, A_j, alpha_j] = imm_nr_reinit( ...
   x_ip, P_ip, MU_ip, p_ij, a_j, ind, dims)
-%EIMM_REINIT Model-conditioned reinitialization step
+%IMM_NR_REINIT Model-conditioned reinitialization step
 %
 % Syntax:
-%   [X_p, P_p, A_j, alpha_j] = EIMM_REINIT(x_ip, P_ip, MU_ip, p_ij, ind, dims)
+%   [x_0j, P_0j, A_j, alpha_j] = IMM_NR_REINIT(x_ip, P_ip, MU_ip, p_ij,
+%                                              ind, dims)
 %
 % In:
 %   x_ip  - Cell array containing N^j x 1 mean state estimate vector for
@@ -23,14 +24,14 @@ function [x_0j, P_0j, A_j, alpha_j] = imm_nr_reinit( ...
   m = size(x_ip,2);
 
   % Compute a_{k-1}^{n}
-  a_j = zeros(1,m);
-  for l = 1:m
-    % Hacky trick
-    if MU_ip(l) < 1e-1
-      MU_ip(l) = 1e-1;
-    end
-    a_j(l) = -log(MU_ip(l));
-  end
+%  a_j = zeros(1,m);
+%  for l = 1:m
+%    % Hacky trick
+%    if MU_ip(l) < 1e-1
+%      MU_ip(l) = 1e-1;
+%    end
+%    a_j(l) = -log(MU_ip(l));
+%  end
 
   % Compute A_j
   A_j = zeros(1,m);
@@ -38,18 +39,18 @@ function [x_0j, P_0j, A_j, alpha_j] = imm_nr_reinit( ...
     A_j(l) = min(a_j);
   end
 
-  %% Verification step (debug)
-  MU_Aj = zeros(1,m);
-  for l = 1:m
-    for k = 1:m
-      MU_Aj(l) = MU_Aj(l) + p_ij(k,l) * MU_ip(l);
-    end
-    MU_Aj(l) = exp(A_j(l))*MU_Aj(l);
-    if MU_Aj(l) <= 0
-      fprintf('\t*************MU_Aj <= 0**************\n');
-    end
-  end
-  alpha_j_temp = -log(MU_Aj);
+%  %% Verification step (debug)
+%  MU_Aj = zeros(1,m);
+%  for l = 1:m
+%    for k = 1:m
+%      MU_Aj(l) = MU_Aj(l) + p_ij(k,l) * MU_ip(l);
+%    end
+%    MU_Aj(l) = exp(A_j(l))*MU_Aj(l);
+%    if MU_Aj(l) <= 0
+%      fprintf('\t*************MU_Aj <= 0**************\n');
+%    end
+%  end
+%  alpha_j_temp = -log(MU_Aj);
 
   % Compute alpha_j
   alpha_j = zeros(1,m);
@@ -58,16 +59,16 @@ function [x_0j, P_0j, A_j, alpha_j] = imm_nr_reinit( ...
       alpha_j(l) = alpha_j(l) + p_ij(k,l)*exp(-(a_j(k)-A_j(l)));
     end
     alpha_j(l) = -log(alpha_j(l));
-    if alpha_j(l) < 0
-      fprintf('\t*************alpha_j < 0**************\n');
-    end
+%    if alpha_j(l) < 0
+%      fprintf('\t*************alpha_j < 0**************\n');
+%    end
   end
 
   % Mixing probabilities
   MU_ij = zeros(m,m);
   for l = 1:m
     for k = 1:m
-        MU_ij(k,l) = p_ij(k,l) * exp(-(a_j(k) - A_j(l) + alpha_j(l)));
+        MU_ij(l,k) = p_ij(l,k) * exp(-(a_j(l) - A_j(k) + alpha_j(k)));
     end
   end
 
