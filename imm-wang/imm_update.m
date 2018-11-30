afa8417046812995d311525571f22c92f9106403
Yang Wang <wang701@purdue.edu>
2018-11-30 14:12:14 -0500

    Fix: lower bound value in imm_update

--- imm-wang/imm_update.m
+++ imm-wang/imm_update.m
@@ -1,5 +1,4 @@
-function [L, imm_mu_k] = imm_update(imm_mu_km1, z_jbar, S_j, c_j, ...
-  sensor_cur, sensor_mu, sensor_var, outage)
+function [L, imm_mu_k] = imm_update(imm_mu_km1, z_jbar, S_j, c_j, l)
 
   % Get the number of models
   m = length(imm_mu_km1);
@@ -7,31 +6,23 @@ function [L, imm_mu_k] = imm_update(imm_mu_km1, z_jbar, S_j, c_j, ...
   % Compute likelihood for each model
   L = nan(1,m);
 
-  if outage == 1
-    sensor_cdf = nan(1,m);
-    for k = 1:m
-      sensor_cdf(k) = normcdf(sensor_cur, sensor_mu(k), sqrt(sensor_var(k)));
-    end
-    [~, I] = max(sensor_cdf);
-    imm_mu_k = 0.05 * ones(1,m);
-    imm_mu_k(I) = 1 - sum(imm_mu_k(~I));
-  else
-    for k = 1:m
-      L_log = -0.5 * log(det(2*pi*S_j{k})) - ...
-        0.5 * z_jbar{k}' * (S_j{k} \ z_jbar{k});
-      L(k) = exp(L_log);
-    end
-
-    if (any(abs(round(L)) == 0))
-      L(1) = 0.00001;
-      L(2) = 0.000001;
-    end
-
-    % Compute normalization constant
-    c = sum(L .* c_j);
-
-    % Compute updated model probabilities
-    imm_mu_k = L .* c_j / c;
+  for k = 1:m
+    L_log = -0.5 * log(det(2*pi*S_j{k})) - ...
+      0.5 * z_jbar{k}' * (S_j{k} \ z_jbar{k});
+    L(k) = exp(L_log);
   end
+
+  % Set lowerbound on likelihood values
+  % Assumption: NCV model always wins
+  if (any(abs(L) < 1e-5)) & (l > 1)
+    L(1) = 0.00001;
+    L(2) = 0.000001;
+  end
+
+  % Compute normalization constant
+  c = sum(L .* c_j);
+
+  % Compute updated model probabilities
+  imm_mu_k = L .* c_j / c;
 
 end %EOF
